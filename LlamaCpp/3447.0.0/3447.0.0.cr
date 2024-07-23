93f2ea47d93378f9bf54187e1fb70210b62e5c89
Fulgurance <zohran.londais@gmail.com>
2024-07-23 23:25:36 +0100

    Added llamacpp installer

---
+++ LlamaCpp/3447.0.0/3447.0.0.cr
@@ -0,0 +1,40 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        if option("Pass1")
+            configureSource(arguments:  "--prefix=/usr  \
+                                        --enable-shared \
+                                        --without-ensurepip",
+                            path:       buildDirectoryPath)
+        else
+            configureSource(arguments:  "--prefix=/usr      \
+                                        --enable-shared     \
+                                        --with-system-expat \
+                                        --with-system-ffi   \
+                                        --enable-optimizations",
+                            path:       buildDirectoryPath)
+        end
+    end
+    
+    def build
+        super
+
+        makeSource(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        makeSource( arguments:  "DESTDIR=#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath} install",
+                    path:       buildDirectoryPath)
+
+        if !option("Pass1")
+            makeLink(   target: "python3",
+                        path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}usr/bin/python",
+                        type:   :symbolicLink)
+        end
+    end
+
+end
